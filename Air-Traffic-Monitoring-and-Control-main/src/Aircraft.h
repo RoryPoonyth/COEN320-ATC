b4c32c859e7c19b25bd848eae8956cff69e85544
Rory Poonyth <66537306+RoryPoonyth@users.noreply.github.com>
2024-11-04 13:59:47 -0500

    Update Aircraft.h

--- Air-Traffic-Monitoring-and-Control-main/src/Aircraft.h
+++ Air-Traffic-Monitoring-and-Control-main/src/Aircraft.h
@@ -1,77 +1,74 @@
-#include <pthread.h>
+#pragma once
+
 #include <iostream>
 #include <string>
-#include <unistd.h>
+#include <thread>
+#include <atomic>
+#include <chrono>
 #include <sys/neutrino.h>
-#ifndef SRC_AIRCRAFT_H_
-#define SRC_AIRCRAFT_H_
 
-using namespace std;
-
-typedef struct {
-	struct _pulse header;
-	int flightId;
-	int positionX;
-	int positionY;
-	int positionZ;
-
-	int speedX;
-	int speedY;
-	int speedZ;
-} AircraftData;
+struct AircraftData {
+    struct _pulse header;
+    int flightId;
+    int positionX;
+    int positionY;
+    int positionZ;
+    int speedX;
+    int speedY;
+    int speedZ;
+};
 
 class Aircraft {
 private:
-	int flightId = 0;
-	int positionX = 0;
-	int positionY = 0;
-	int positionZ = 0;
-	int speedX = 0;
-	int speedY = 0;
-	int speedZ = 0;
-	int time = 0; // We can discuss which type this variable should be
-	int err_no = 0;
-	pthread_attr_t attr;
-	pthread_t thread;
+    int flightId = 0;
+    int positionX = 0;
+    int positionY = 0;
+    int positionZ = 0;
+    int speedX = 0;
+    int speedY = 0;
+    int speedZ = 0;
+    int time = 0; // Consider using std::chrono for more flexible time management
+
+    std::thread thread;
+    std::atomic<bool> isRunning{false};
 
 public:
-	Aircraft();
-	Aircraft(int flightId, int positionX, int positionY, int positionZ, int speedX, int speedY, int speedZ, int time);
-	virtual ~Aircraft();
+    Aircraft();
+    Aircraft(int flightId, int positionX, int positionY, int positionZ, int speedX, int speedY, int speedZ, int time);
+    virtual ~Aircraft();
 
-	int getFlightId();
-	void setFlightId(int flightId);
+    int getFlightId() const;
+    void setFlightId(int flightId);
 
-	int getPositionX();
-	int getPositionY();
-	int getPositionZ();
-	int getPrevPositionX();
-	int getPrevPositionY();
-	int getPrevPositionZ();
-	void setPositionX(int positionX);
-	void setPositionY(int positionY);
-	void setPositionZ(int positionZ);
+    int getPositionX() const;
+    int getPositionY() const;
+    int getPositionZ() const;
+    int getPrevPositionX() const;
+    int getPrevPositionY() const;
+    int getPrevPositionZ() const;
+    void setPositionX(int positionX);
+    void setPositionY(int positionY);
+    void setPositionZ(int positionZ);
 
-	int getSpeedX();
-	int getSpeedY();
-	int getSpeedZ();
-	void setSpeedX(int speedX);
-	void setSpeedY(int speedY);
-	void setSpeedZ(int speedZ);
+    int getSpeedX() const;
+    int getSpeedY() const;
+    int getSpeedZ() const;
+    void setSpeedX(int speedX);
+    void setSpeedY(int speedY);
+    void setSpeedZ(int speedZ);
 
-	void startThread();
-	void stopThread();
+    void startThread();
+    void stopThread();
+    void move();
 
-	static void *planeOperations(void *arg);
-	static void* aircraftListenHelper(void* args);
-
-	void move();
-	void* listen(string attachPoint);
+private:
+    void planeOperations();
+    void listen(const std::string& attachPoint);
 };
 
 struct AircraftListenArgs {
-	Aircraft* aircraft;
-	string attachPoint;
+    Aircraft* aircraft;
+    std::string attachPoint;
 };
 
 #endif /* SRC_AIRCRAFT_H_ */

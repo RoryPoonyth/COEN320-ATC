f348f7297e00a412e38c649152b49380b410d523
Rory Poonyth <66537306+RoryPoonyth@users.noreply.github.com>
2024-11-04 14:15:33 -0500

    Update Radar.cpp

--- Air-Traffic-Monitoring-and-Control-main/src/Radar.cpp
+++ Air-Traffic-Monitoring-and-Control-main/src/Radar.cpp
@@ -4,61 +4,46 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <stdio.h>
 #include <sys/dispatch.h>
+#include <cerrno>
+#include <cstring>
 
-using namespace std;
-
-#define ATTACH_POINT "default"
+constexpr char ATTACH_POINT[] = "default";
 
 Radar::Radar() {}
 
-Radar::Radar(vector<Aircraft>& aircrafts) {
-    this->aircrafts = aircrafts;
-}
+Radar::Radar(const std::vector<Aircraft>& aircrafts) : aircrafts(aircrafts) {}
 
-Radar::Radar(Aircraft& aircraft) {
-    this->aircraft = aircraft;
-}
+Radar::Radar(const Aircraft& aircraft) : aircraft(aircraft) {}
 
 void* Radar::radarInitializer(void* args) {
-    Radar* radar = (Radar*)args;
+    auto* radar = static_cast<Radar*>(args);
     radar->pingAircraft();
-    return NULL;
+    return nullptr;
 }
 
 void* Radar::pingAircraft() {
-    string attachPoint = string(ATTACH_POINT) + "_RADAR";
-
-    name_attach_t* attach;
-
-    if ((attach = name_attach(NULL, attachPoint.c_str(), 0)) == NULL) {
-        perror("Radar (pingAircraft): Error occurred while creating the attach point");
+    std::string attachPoint = std::string(ATTACH_POINT) + "_RADAR";
+    name_attach_t* attach = name_attach(nullptr, attachPoint.c_str(), 0);
+    if (!attach) {
+        std::cerr << "Radar (pingAircraft): Error occurred while creating the attach point: " << strerror(errno) << std::endl;
+        return (void*)EXIT_FAILURE;
     }
 
     AircraftData aircraftCommand;
     while (true) {
-        int rcvid = MsgReceive(attach->chid, &aircraftCommand, sizeof(aircraftCommand), NULL);
-
+        int rcvid = MsgReceive(attach->chid, &aircraftCommand, sizeof(aircraftCommand), nullptr);
         if (rcvid == -1) {
             break;
         }
 
-        if (rcvid == 0) { /* Pulse received */
-            switch (aircraftCommand.header.code) {
-                case _PULSE_CODE_DISCONNECT:
-                    ConnectDetach(aircraftCommand.header.scoid);
-                    continue;
-                case _PULSE_CODE_UNBLOCK:
-                    break;
-                default:
-                    break;
-            }
+        if (rcvid == 0) { // Pulse received
+            handlePulse(aircraftCommand);
             continue;
         }
 
         if (aircraftCommand.header.type == _IO_CONNECT) {
-            MsgReply(rcvid, EOK, NULL, 0);
+            MsgReply(rcvid, EOK, nullptr, 0);
             continue;
         }
 
@@ -68,50 +53,65 @@ void* Radar::pingAircraft() {
         }
 
         if (aircraftCommand.header.type == 0x08 && aircraftCommand.header.subtype == 0x08) {
-            string attachPoint = string(ATTACH_POINT) + "_" + to_string(aircraftCommand.flightId);
-
-            int server_coid;
-            if ((server_coid = name_open(attachPoint.c_str(), 0)) == -1) {
-                perror("Error occurred while attaching the channel");
-            }
-
-            AircraftData aircraftCommand;
-            aircraftCommand.header.type = 0x00;
-            aircraftCommand.header.subtype = 0x01;
-
-            if (MsgSend(server_coid, &aircraftCommand, sizeof(aircraftCommand), &aircraftCommand, sizeof(aircraftCommand)) == -1) {
-                cout << "Error while sending the message for aircraft: " << strerror(errno) << endl;
-                name_close(server_coid);
-            }
-
-            MsgReply(rcvid, EOK, &aircraftCommand, sizeof(aircraftCommand));
-            continue;
+            handleAircraftRequest(rcvid, aircraftCommand);
         } else {
             MsgError(rcvid, ENOSYS);
-            continue;
         }
     }
 
+    name_detach(attach, 0);
     return EXIT_SUCCESS;
 }
 
-AircraftData Radar::operatorRequestPingAircraft(int flightId) {
-    string attachPoint = string(ATTACH_POINT) + "_" + to_string(flightId);
+void Radar::handlePulse(const AircraftData& aircraftCommand) const {
+    switch (aircraftCommand.header.code) {
+        case _PULSE_CODE_DISCONNECT:
+            ConnectDetach(aircraftCommand.header.scoid);
+            break;
+        default:
+            break;
+    }
+}
 
-    int server_coid;
-    if ((server_coid = name_open(attachPoint.c_str(), 0)) == -1) {
-        perror("Error occurred while attaching the channel");
+void Radar::handleAircraftRequest(int rcvid, AircraftData& aircraftCommand) const {
+    std::string attachPoint = std::string(ATTACH_POINT) + "_" + std::to_string(aircraftCommand.flightId);
+    int server_coid = name_open(attachPoint.c_str(), 0);
+    if (server_coid == -1) {
+        std::cerr << "Error occurred while attaching to channel: " << strerror(errno) << std::endl;
+        MsgError(rcvid, ENOSYS);
+        return;
     }
 
-    AircraftData aircraftCommand;
     aircraftCommand.header.type = 0x00;
     aircraftCommand.header.subtype = 0x01;
 
     if (MsgSend(server_coid, &aircraftCommand, sizeof(aircraftCommand), &aircraftCommand, sizeof(aircraftCommand)) == -1) {
-        cout << "Error while sending the message for aircraft: " << strerror(errno) << endl;
+        std::cerr << "Error while sending message to aircraft: " << strerror(errno) << std::endl;
         name_close(server_coid);
+    } else {
+        MsgReply(rcvid, EOK, &aircraftCommand, sizeof(aircraftCommand));
     }
 
+    name_close(server_coid);
+}
+
+AircraftData Radar::operatorRequestPingAircraft(int flightId) const {
+    std::string attachPoint = std::string(ATTACH_POINT) + "_" + std::to_string(flightId);
+    int server_coid = name_open(attachPoint.c_str(), 0);
+    if (server_coid == -1) {
+        std::cerr << "Error occurred while attaching to channel: " << strerror(errno) << std::endl;
+        return {};
+    }
+
+    AircraftData aircraftCommand{};
+    aircraftCommand.header.type = 0x00;
+    aircraftCommand.header.subtype = 0x01;
+
+    if (MsgSend(server_coid, &aircraftCommand, sizeof(aircraftCommand), &aircraftCommand, sizeof(aircraftCommand)) == -1) {
+        std::cerr << "Error while sending message to aircraft: " << strerror(errno) << std::endl;
+    }
+
+    name_close(server_coid);
     return aircraftCommand;
 }
 

8627c57be167748e65011a33c641041c28d47dd4
Rory Poonyth <66537306+RoryPoonyth@users.noreply.github.com>
2024-11-04 13:53:51 -0500

    Update ATC.cpp

--- Air-Traffic-Monitoring-and-Control-main/src/ATC.cpp
+++ Air-Traffic-Monitoring-and-Control-main/src/ATC.cpp
@@ -1,87 +1,118 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <memory>
+#include <chrono>
 #include "Aircraft.h"
 #include "Radar.h"
 #include "CommunicationSystem.h"
 #include "DataDisplay.h"
 #include "ComputerSystem.h"
 #include "OperatorConsole.h"
-#include <vector>
-#include <fstream>
 
-#define ROWS 25
-#define COLUMNS 25
-
-#define ATTACH_POINT "default"
+constexpr int ROWS = 25;
+constexpr int COLUMNS = 25;
+constexpr char ATTACH_POINT[] = "default";
 
 using namespace std;
+using namespace std::chrono_literals;
 
-void createInput() { // Call createInput() if you need to modify the input file
-    fstream file;
-    file.open("low.txt", ios::out); // Relative path (you can find the file in /data/var/tmp in the virtual machine)
-    if (file.is_open()) {
-        file << "3, 0, 0, 0, 250, 250, 0, 0\n";
-        file << "4, 5000, 0, 0, 250, 0, 0, 0\n";
-        file << "5, 1800, 0, 0, 251, 0, 0, 0\n";
-        file.close();
+void createInputFile(const string& filename, const vector<string>& data) {
+    ofstream file(filename);
+    if (!file) {
+        cerr << "Error creating file: " << filename << endl;
+        return;
     }
-
-    file.open("med.txt", ios::out);
-    if (file.is_open()) {
-        file << "3, 0, 0, 0, 250, 250, 0, 0\n";
-        file << "4, 5000, 0, 0, 250, 0, 0, 0\n";
-        file << "5, 1800, 0, 0, 251, 0, 0, 0\n";
-        file << "6, 0, 700, 0, 300, 0, 0, 0\n";
-        file << "7, 1000, 3000, 0, 500, 0, 0, 0\n";
-        file.close();
-    }
-
-    file.open("high.txt", ios::out);
-    if (file.is_open()) {
-        file << "3, 0, 0, 0, 250, 250, 0, 0\n";
-        file << "4, 5000, 0, 0, 250, 0, 0, 0\n";
-        file << "5, 1800, 0, 0, 251, 0, 0, 0\n";
-        file << "6, 0, 700, 0, 300, 0, 0, 0\n";
-        file << "7, 1000, 3000, 0, 500, 0, 0, 0\n";
-        file << "8, 4000, 700, 0, 300, 0, 0, 0\n";
-        file << "9, 1000, 200, 0, 500, 0, 0, 0\n";
-        file.close();
+    for (const auto& line : data) {
+        file << line << '\n';
     }
 }
 
-void printInput(const string& filename) { // Call printInput() if you need to verify that the input file was created
-    fstream file;
-    file.open(filename, ios::in);
-    if (file.is_open()) {
-        string line;
-        while (getline(file, line)) {
-            cout << line << endl;
-        }
-        file.close();
+void createInputFiles() {
+    createInputFile("low.txt", {
+        "3, 0, 0, 0, 250, 250, 0, 0",
+        "4, 5000, 0, 0, 250, 0, 0, 0",
+        "5, 1800, 0, 0, 251, 0, 0, 0"
+    });
+
+    createInputFile("med.txt", {
+        "3, 0, 0, 0, 250, 250, 0, 0",
+        "4, 5000, 0, 0, 250, 0, 0, 0",
+        "5, 1800, 0, 0, 251, 0, 0, 0",
+        "6, 0, 700, 0, 300, 0, 0, 0",
+        "7, 1000, 3000, 0, 500, 0, 0, 0"
+    });
+
+    createInputFile("high.txt", {
+        "3, 0, 0, 0, 250, 250, 0, 0",
+        "4, 5000, 0, 0, 250, 0, 0, 0",
+        "5, 1800, 0, 0, 251, 0, 0, 0",
+        "6, 0, 700, 0, 300, 0, 0, 0",
+        "7, 1000, 3000, 0, 500, 0, 0, 0",
+        "8, 4000, 700, 0, 300, 0, 0, 0",
+        "9, 1000, 200, 0, 500, 0, 0, 0"
+    });
+}
+
+void printFileContent(const string& filename) {
+    ifstream file(filename);
+    if (!file) {
+        cerr << "Error opening file: " << filename << endl;
+        return;
     }
+    string line;
+    while (getline(file, line)) {
+        cout << line << endl;
+    }
+}
+
+vector<unique_ptr<Aircraft>> loadAircraftsFromFile(const string& filename) {
+    ifstream file(filename);
+    vector<unique_ptr<Aircraft>> aircrafts;
+
+    if (!file) {
+        cerr << "Error opening file: " << filename << endl;
+        return aircrafts;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        vector<int> args;
+        size_t pos;
+        while ((pos = line.find(',')) != string::npos) {
+            args.push_back(stoi(line.substr(0, pos)));
+            line.erase(0, pos + 1);
+        }
+        args.push_back(stoi(line));
+
+        if (args.size() == 8) {
+            aircrafts.push_back(make_unique<Aircraft>(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]));
+        } else {
+            cerr << "Invalid line in file: " << line << endl;
+        }
+    }
+    return aircrafts;
 }
 
 int main() {
-    string filename = "";
     int congestion = 0;
-    cout << "Type 1 for low, 2 for medium, 3 for high congestion: " << endl;
+    cout << "Select congestion level (1 = Low, 2 = Medium, 3 = High): ";
     cin >> congestion;
+
+    string filename;
     switch (congestion) {
-        case 1:
-            filename = "low.txt";
-            break;
-        case 2:
-            filename = "med.txt";
-            break;
-        case 3:
-            filename = "high.txt";
-            break;
+        case 1: filename = "low.txt"; break;
+        case 2: filename = "med.txt"; break;
+        case 3: filename = "high.txt"; break;
         default:
-            cout << "Invalid option" << endl;
+            cerr << "Invalid option" << endl;
             return 1;
     }
 
-    createInput();
-    printInput(filename);
+    createInputFiles();
+    printFileContent(filename);
 
     DataDisplay dataDisplay;
     ComputerSystem computerSystem;
@@ -89,53 +120,38 @@ int main() {
     Radar radar;
     CommunicationSystem communicationSystem;
 
-    vector<Aircraft*> aircrafts;
-
-    fstream file;
-    file.open(filename, ios::in);
-    if (file.is_open()) {
-        string line;
-        while (getline(file, line)) {
-            string delimiter = ",";
-            size_t pos = 0;
-            string token;
-            vector<int> args;
-
-            while ((pos = line.find(delimiter)) != string::npos) {
-                token = line.substr(0, pos);
-                args.push_back(stoi(token));
-                line.erase(0, pos + delimiter.length());
-            }
-            args.push_back(stoi(line));
-
-            aircrafts.push_back(new Aircraft(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]));
-        }
-        file.close();
-    }
-
+    auto aircrafts = loadAircraftsFromFile(filename);
     computerSystem.setAircrafts(aircrafts);
     computerSystem.alarm();
-    for (Aircraft* ap : aircrafts) {
-        ap->startThread();
+
+    vector<thread> threads;
+    threads.emplace_back(&Radar::radarInitializer, &radar);
+    threads.emplace_back(&DataDisplay::MapInitializer, &dataDisplay);
+    threads.emplace_back(&ComputerSystem::MapDisplayThread, &computerSystem);
+    threads.emplace_back(&DataDisplay::dataDisplayInitializer, &dataDisplay);
+    threads.emplace_back(&ComputerSystem::computerSystemThread, &computerSystem);
+    threads.emplace_back(&OperatorConsole::listenForOperatorInput, &operatorConsole);
+    threads.emplace_back(&ComputerSystem::separationCheckThread, &computerSystem);
+
+    // Start aircraft threads
+    for (auto& aircraft : aircrafts) {
+        threads.emplace_back(&Aircraft::startThread, aircraft.get());
     }
 
-    // Start the threads
-    pthread_t mapDisplayThread, userInputThread, computerSystemThread, dataDisplayThread, mapInitializeThread, separationThread, radarThread;
-    pthread_create(&radarThread, nullptr, &Radar::radarInitializer, &radar);
-    pthread_create(&mapInitializeThread, nullptr, &DataDisplay::MapInitializer, &dataDisplay);
-    pthread_create(&mapDisplayThread, nullptr, &ComputerSystem::MapDisplayThread, &computerSystem);
-    pthread_create(&dataDisplayThread, nullptr, &DataDisplay::dataDisplayInitializer, &dataDisplay);
-    pthread_create(&computerSystemThread, nullptr, &ComputerSystem::computerSystemThread, &computerSystem);
-    pthread_create(&userInputThread, nullptr, &OperatorConsole::listenForOperatorInput, &operatorConsole);
-    pthread_create(&separationThread, nullptr, &ComputerSystem::separationCheckThread, &computerSystem);
-
-    // Run infinitely, otherwise main will complete its execution before the threads and program will terminate
+    // Run infinitely with periodic delay to reduce resource use
     while (true) {
-        sleep(500); // Reduce resource use
+        this_thread::sleep_for(500ms);
     }
 
-    for (Aircraft* aircraft : aircrafts) {
+    // Clean up and stop all threads (not reached in this infinite loop)
+    for (auto& aircraft : aircrafts) {
         aircraft->stopThread();
+    }
+
+    for (auto& t : threads) {
+        if (t.joinable()) {
+            t.join();
+        }
     }
 
     return 0;
